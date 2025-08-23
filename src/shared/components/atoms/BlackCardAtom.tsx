d3f4e6f7cfbb60398510410c8be1c85a9106db43
Manish <manishoff328@gmail.com>
2025-08-23 21:44:43 +0530

    modified vite.config file for deployment

--- src/shared/components/atoms/BlackCardAtom.tsx
+++ src/shared/components/atoms/BlackCardAtom.tsx
@@ -9,7 +9,6 @@ export default function BlackCardAtom({
   line1,
   line2,
   line3,
-  chatIconSrc,
 }: BlackCardAtomProps) {
   return (
     <div className="flex items-end w-full mt-6 relative">
@@ -18,15 +17,6 @@ export default function BlackCardAtom({
         <p className="text-[16px] md:text-[24px] mb-2 font-bold">{line1}</p>
         <p className="text-[16px] md:text-[24px] mb-2 font-bold">{line2}</p>
         <p className="text-[16px] md:text-[12px]">{line3}</p>
-      </div>
-
-      {/* Chat Icon outside the card */}
-      <div className=" flex-shrink-0 rounded-full p-2 bg-white shadow-lg cursor-pointer self-end">
-        <img
-          src={chatIconSrc}
-          alt="Chat Icon"
-          className="w-12 h-12 rounded-full"
-        />
       </div>
     </div>
   );
